d5851f34866b3b1581240bd95db28c8d7b312866
Suhas Lokesha <suhas.l.93@gmail.com>
2024-02-28 11:09:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import "./styles.css";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filterData, setFilterData] = useState([]);
+  const [isSearch, setIsSearch] = useState(false);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((dta) => setData(dta))
+      .catch((error) => console.log("Error in fetching data", error));
+  }, []);
+
+  useEffect(() => {
+    if (search.length > 0) {
+      setFilterData(
+        data.filter((country) =>
+          country.name.common.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+      setIsSearch(true);
+    } else {
+      setIsSearch(false);
+    }
+  }, [search]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder={"Search for countries"}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {isSearch ? (
+        <div className="container">
+          {filterData.map((country) => {
+            return (
+              <div key={country.ccn3} className="card">
+                <img
+                  src={country.flags.png}
+                  alt={`Flag of ${country.name.common}`}
+                  className="flag-img"
+                />
+                <h3> {country.name.common} </h3>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="container">
+          {data.map((country) => {
+            return (
+              <div key={country.ccn3} className="card">
+                <img
+                  src={country.flags.png}
+                  alt={`Flag of ${country.name.common}`}
+                  className="flag-img"
+                />
+                <h3> {country.name.common} </h3>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="container">
+        {data.map((country) => {
+          return (
+            <div key={country.ccn3} className="card">
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                className="flag-img"
+              />
+              <h3> {country.name.common} </h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
