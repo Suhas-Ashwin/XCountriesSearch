eebffac36089250519695a60a9a5f55b46c1e391
Suhas Lokesha <suhas.l.93@gmail.com>
2024-02-29 20:21:27 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -328,7 +328,7 @@ export default function App() {
               );
             })
           ) : (
-            <p> No matching countries</p>
+            null
           )
         ) : (
           data.map((country) => {
